1a7c76c537f666f3727a5f826036b4982c0bd50b
Kulwinder2104 <128737946+Kulwinder2104@users.noreply.github.com>
2024-05-03 08:35:54 -0500

    chapter

---
+++ sandbox.js
@@ -0,0 +1,38 @@
+//Variables can only start with uderscore or doller we cnt start it by a number
+// we cnt use let and const together
+
+let age = 25;
+let year = 2019;
+// Let is used when we want to make chnge in the variables in the future
+// Generaly use let
+
+//Comment
+//log things to console
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+// Const is used when we dont want to make any changes in it in future even by a mistake
+// when you dont want to overright then use constant 
+const points = 100;
+
+console.log(points);
+
+
+//var is old keyword or old way 
+var score = 75;
+console.log(score);
+
+// but we will use let and const
+
+/*
+Javascript datatypes:
+Number- 
+String- 'hello' , dhillon@gmail.com  - series of charactes in double quotes
+Boolean- specail logical true or false values 
+Null- a Way we cam explicitly set a variable with no value - a variables dont have value yet- we do
+Undefined- For variables that have not yet been defined- automatically null
+Object- Comples data structure- Array, Dates, Literals etc - whole subset
+Symbol- used with objects - new addition to javascript
+*/
